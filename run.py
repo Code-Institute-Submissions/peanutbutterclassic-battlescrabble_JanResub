1f2a78bc8183db88f57a0aaf0fab19c2d0999d7e
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2021-08-15 11:19:18 +0000

    Import gspread and google oauth credentials to workspace

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-# python code goes here
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
