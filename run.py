8b0ea18ee74bbd053da03119b8ff6b4155437ad3
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2021-08-16 16:29:38 +0000

    Create battleships class

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import random 
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -12,22 +13,56 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("battleship-words")
 
-# Create battle grid
-
-board = []
+# Create battle grid (10x10) using initial empty sting.
+    
+board = [] 
 
 for x in range(0, 10):
     board.append(["0"] * 10)
 
-def print_board(board):
-    for i in board:
-        print(" ".join(i))
-print("You are entering battle perimetre!")
-print("Fire right away!\n")    
-print_board(board)
+    # Create function to build grid for battle.
+    def print_board(board):
+        """
+        Create function to build grid for battle.
+        """
+
+        for i in board:
+            print(" ".join(i))
+    print("You are entering battle perimetre!")
+    print("Fire right away!\n")    
+    print_board(board)
+
+class Battle_Ships:
+    """
+    Create battleships length 3, 4 and 5
+    """
+    def battleships(self, ship_three, ship_four, ship_five):
+
+    
+
+#     while num_of_ships_placed != num_of_ships:
+#         random_row = random.randint(0, rows - 1)
+#         random_col = random.randint(0, cols - 1)
+#         direction = random.choice(["left", "right", "up", "down"])
+#         ship_size = random.randint(3, 5)
+#         if try_to_place_ship_on_grid(random_row, random_col, direction, ship_size):
+#             num_of_ships_placed += 1
+
+# battle_perimeter()
+
+# Create battle ships.
+# Three types of ships: (1) ship with length = 3 (2) ship with length = 4 (3) ship with length = 5.
+battleships = []
+
+
+
+
+
 
 # Create a function to retrieve a random word from each column in the worksheet.
-
+"""
+Function is duplicated three times for (1) three letter word (2) four letter word (3) five letter word.
+"""
 def three_letter_words():
     """
     Retrieve a randomised three letter word from data in google sheet.
@@ -42,6 +77,7 @@ def three_letter_words():
     data = battle_words.col_values(1)
     print(data)
 
+    # Input method shall have \n for heroku compatibility.
     user_three_str = input("Choose your word wisely to sink the ship: \n")
     print(f"You have selected {user_three_str}") 
     
@@ -61,7 +97,8 @@ def four_letter_words():
     battle_words = SHEET.worksheet("battle-words")
     data = battle_words.col_values(2)
     print(data)
-
+    
+    # Input method shall have \n for heroku compatibility.
     user_four_str = input("Choose your word wisely to sink the ship: \n")
     print(f"You have selected {user_four_str}")
     
@@ -80,7 +117,8 @@ def five_letter_words():
     battle_words = SHEET.worksheet("battle-words")
     data = battle_words.col_values(3)
     print(data)
-
+    
+    # Input method shall have \n for heroku compatibility.
     user_five_str = input("Choose your word wisely to sink the ship: \n")
     print(f"You have selected {user_five_str}")
     has_five = user_five_str in data
